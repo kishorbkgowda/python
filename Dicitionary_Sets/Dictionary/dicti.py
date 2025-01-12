008d76606cbf4d5383d2b55f653b77d464b8e9f7
kishorbkgowda <.>
2025-01-12 17:24:56 +0530

    svd

---
+++ Dicitionary_Sets/Dictionary/dicti.py
@@ -0,0 +1,6 @@
+marks = {1:"Kishor B K",2:"Lol",2:"Lol"}
+marks.update({1:"Kunni"})
+print(marks.items())
+print(marks.get(1))
+
+
---
+++ Dicitionary_Sets/Dictionary/practise.py
@@ -0,0 +1,26 @@
+lol = {"name":"Kishor B K","age":24,"city":"Mangalore"}
+
+print(lol["name"])
+
+lol.update({"country":"USA"})
+
+lol.update({"age":35})
+
+lol.pop("city")
+
+print(lol.keys())
+
+print(lol.__contains__("age"))
+
+print(lol.values())
+
+keys = ["name", "age", "city"]
+values = ["Alice", 30, "New York"]
+hi = {}
+hi.update({keys[0]:values[0],keys[1]:values[1],keys[2]:values[2]})
+
+print(hi.items())
+
+print(len(hi))
+
+print(lol.items())
