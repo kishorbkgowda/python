3855811123b86733c69f4d0aa97e2cefe7a93200
kishorbkgowda <.>
2025-01-12 17:36:09 +0530

    lol

--- Dicitionary_Sets/Dictionary/practise.py
+++ Dicitionary_Sets/Dictionary/practise.py
@@ -1,26 +1,31 @@
-lol = {"name":"Kishor B K","age":24,"city":"Mangalore"}
+# lol = {"name":"Kishor B K","age":24,"city":"Mangalore"}
 
-print(lol["name"])
+# print(lol["name"])
 
-lol.update({"country":"USA"})
+# lol.update({"country":"USA"})
 
-lol.update({"age":35})
+# lol.update({"age":35})
 
-lol.pop("city")
+# lol.pop("city")
 
-print(lol.keys())
+# print(lol.keys())
 
-print(lol.__contains__("age"))
+# print(lol.__contains__("age"))
 
-print(lol.values())
+# print(lol.values())
 
-keys = ["name", "age", "city"]
-values = ["Alice", 30, "New York"]
-hi = {}
-hi.update({keys[0]:values[0],keys[1]:values[1],keys[2]:values[2]})
+# keys = ["name", "age", "city"]
+# values = ["Alice", 30, "New York"]
+# hi = {}
+# hi.update({keys[0]:values[0],keys[1]:values[1],keys[2]:values[2]})
 
-print(hi.items())
+# print(hi.items())
 
-print(len(hi))
+# print(len(hi))
 
-print(lol.items())
+# print(lol.items())
+
+s = [8, 7, 12, "Harry", [1,2]]
+
+s[4][0] = 9
+print(s)
