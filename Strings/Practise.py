6a75969436668568112a475f5773a6b1e7029b9e
kishorbkgowda <.>
2025-01-12 10:16:55 +0530

    practised some question

---
+++ Strings/Practise.py
@@ -0,0 +1,31 @@
+#How can you find the length of a string in Python?
+a = "Hello"
+print(len(a))
+
+#What is the output of the expression "Hello"[1]
+b = "Hello"[1]
+print(b)
+
+#How can you extract a substring from a string using slicing?
+c = "Kishor"
+print(c[1:3])
+
+#How do you concatenate two strings in Python?
+d = "Hello"
+e="World"
+print(d+" "+e)
+
+#What is the result of "Python" * 3?
+print("Python" * 3)
+
+#How can you convert a string to lowercase in Python?
+print("Python".lower())
+
+#How do you check if a string starts with a specific character or word?
+print(d.startswith("Hel"))
+
+#How can you find the index of a specific character in a string?
+print(d.find('e'))
+
+#What happens if you try to access an index that is out of range in a string?
+print(d[6])
